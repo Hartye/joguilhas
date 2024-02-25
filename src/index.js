e9531a45a00ff1a191479fe568436ec0b5fa23ee
Hartye <hartye.lilith@gmail.com>
2024-02-25 20:00:14 +0000

    Adding chat with socket

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import bodyParser from "body-parser";
 import { fileURLToPath } from "url"
 import path from "path";
@@ -13,7 +15,20 @@ const router = express.Router();
 const baseURL = process.env.PORT || 3000;
 const __fileName = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__fileName);
+const app = createServer(server);
+const io = new Server(app);
 
+// Socket
+io.on('connection', (socket) => {
+    console.log(("User connected: " + socket.id));
+
+    socket.on('message', (data) => {
+        console.log("Message: " + data + "\nFrom: " + socket.id);
+        io.emit('message', data);
+    });
+});
+
+// Routes
 router.get("/", (req, res) => {
     return res.redirect("/hub");
 });
@@ -24,6 +39,10 @@ router.get("/hub", (req, res) => {
 
 router.get("/games/letras", (req, res) => {
     return res.sendFile(__dirname + "/pages/letras.html");
+});
+
+router.get("/games/chat", (req, res) => {
+    return res.sendFile(__dirname + "/pages/chat.html");
 });
 
 router.post("/add/letra", bodyParser.json(), (req, res) => {
@@ -55,12 +74,16 @@ router.get("/styles/letras.css", (req, res) => {
     return res.sendFile(__dirname + "/styles/letras.css");
 });
 
+router.get("/styles/chat.css", (req, res) => {
+    return res.sendFile(__dirname + "/styles/chat.css");
+});
+
 router.get("/scripts/letras.js", (req, res) => {
     return res.sendFile(__dirname + "/scripts/letras.js");
 });
 
-router.get("/images/exemplo2.png", (req, res) => {
-    return res.sendFile(__dirname + "/images/exemplo2.png");
+router.get("/images/chat.svg", (req, res) => {
+    return res.sendFile(__dirname + "/images/chat.svg");
 });
 
 router.get("/images/abc.png", (req, res) => {
@@ -69,4 +92,4 @@ router.get("/images/abc.png", (req, res) => {
 
 server.use(router)
 
-server.listen(baseURL, () => console.log("Listening to port 3000\nLink: http://localhost:3000/"));
+app.listen(baseURL, () => console.log("Listening to port 3000\nLink: http://localhost:3000/"));
