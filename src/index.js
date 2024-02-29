b16825a35330059d6a792e90079b9098235c396b
Hartye <hartye.lilith@gmail.com>
2024-02-29 19:10:38 +0000

    Finishing chase game

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const __dirname = path.dirname(__fileName);
 const app = createServer(server);
 const io = new Server(app);
 
+const FPS = 30;
 let rooms = [];
 
 // Socket
@@ -34,6 +35,7 @@ io.on('connection', (socket) => {
             ...data,
             id: socket.id
         });
+        console.log(`${socket.id} joined the room`)
         console.log(rooms);
     });
 
@@ -43,15 +45,49 @@ io.on('connection', (socket) => {
             ...data,
             id: socket.id
         }
+        rooms[index].afk = 1800;
+    });
 
+    setInterval(() => {
         io.emit('move', rooms);
-    });
+        rooms.forEach(s => {
+            if (s.afk <= 0) {
+                rooms.splice(rooms.findIndex(i => i.id == s.id), 1);
+                io.emit('dead', s.id);
+            }
+            else {
+                s.afk -= 1;
+            }
+        });
 
-    socket.on('disconnect', () => {
-        rooms.splice(rooms.findIndex(s => s.id == socket.id), 1);
-        console.log(rooms);
-    });
+        rooms.filter(enemies => enemies.enemy === true).forEach(enemy => {
+            rooms.filter(victims => victims.enemy === false).forEach(victim => {
+                if (objectColide(enemy, victim)) {
+                    rooms.splice(rooms.findIndex(i => i.id == victim.id), 1);
+                    io.emit('dead', victim.id);
+                }
+            });
+        });
+    }, 1000 / FPS);
 });
+
+const objectColide = (obj1, obj2) => {
+    for (let i = obj1.posX; i < obj1.posX + obj1.width; i++) {
+        for (let p = obj2.posX; p < obj2.posX + obj1.width; p++) {
+            if (p === i) {
+                for (let j = obj1.posY; j < obj1.posY + obj1.posY; j++) {
+                    for (let w = obj2.posY; w < obj2.posY + obj2.height; w++) {
+                        if (w === j) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return false;
+};
 
 // Routes
 router.get("/", (req, res) => {
